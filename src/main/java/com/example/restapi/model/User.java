59de058c2d01a50c24c25b23fb1d809ce3bd752b
kzsvps <kzsvps950428@gmail.com>
2025-06-22 02:19:26 +0800

    initial commit

---
+++ src/main/java/com/example/restapi/model/User.java
@@ -0,0 +1,39 @@
+package com.example.restapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+    private String password;
+
+    // Getter & Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
